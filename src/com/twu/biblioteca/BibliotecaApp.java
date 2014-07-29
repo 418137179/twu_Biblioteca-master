710d8ee20f582129faae8bb8bea6ea52974fff4e
YangJing <418137179@qq.com>
2014-07-29 14:15:07 +0800

    fix BibliotecaApp's structure.:Yangjing_20140729

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,55 +1,79 @@
 package com.twu.biblioteca;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BibliotecaApp {
 
-    private static Library library = new Library();
-    private static ArrayList<String> faceBook = new ArrayList<String>(Arrays.asList("MainMenu","Check-Out Book","Return Book","Quit"));
-    private static ArrayList<String> mainmenu = new ArrayList<String>(Arrays.asList("List Books","List Movies"));
-    private static BibliotecaApp bibliotecaApp = new BibliotecaApp();
-
+    private static Library library;
+    private static ArrayList<String> faceBook = new ArrayList<String>(Arrays.asList("MainMenu","CheckOut","Return","UserAccounts","Quit"));
+    private static ArrayList<String> superfaceBook = new ArrayList<String>(Arrays.asList("MainMenu","Lend","CheckOut","Return","UserAccounts","Quit"));
+    private static BasicMenu basicmenu = new BasicMenu();
 
     BibliotecaApp(){
+
         Book book1 = new Book("book1","author1","press1");
         Book book2 = new Book("book2","auhtor2","press2");
         Book book3 = new Book("book3","auhtor3","press3");
 
+        Movie movie1 = new Movie("movie1","1942","director1",1);
+        Movie movie2 = new Movie("movie2","1943","director2",2);
+
+        library = new Library();
         library.addBookToLibrary(book1);
         library.addBookToLibrary(book2);
+        library.addBookToLibrary(book3);
+        library.lendbook(book3);
 
+        library.addMovieToLibrary(movie1);
+        library.addMovieToLibrary(movie2);
+        library.lendMovie(movie2);
     }
 
-    public static void main(String[] args) {
-        while(true){
+    public static void main(String[] args) throws Exception{
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+//        People tempLoginUser = new People(library.getLoginUser());
 
-            bibliotecaApp.welcomeMessage("Welcome you to Biblioteca Library!");
-            viewFaceBook(faceBook);
+        while (login()) {
+//            library.setLoginUser(tempLoginUser);
+            while(true) {
+                basicmenu.welcomeMessage("Welcome you to Biblioteca Library!");
 
-            String input = getInput();
-            assert input != null;
+                if ("admin".equals(library.getLoginUser().getDuty())) {
+                    basicmenu.viewFaceBook(superfaceBook);
+                } else
+                    basicmenu.viewFaceBook(faceBook);
 
-            if(input.equals("Quit"))  {
-                System.out.println("GoodByeFace!");
-                break;
-            }
-            else {
-                switch (input) {
-                    case "MainMenu":
-                        mainMenu(input);
-                        break;
-                    case "Check-Out Book":
-                        checkOutBook(input);
-                        break;
-                    case "Return Book":
-                        returnBook(input);
-                        break;
-                    default:
-                        System.out.println("Please confirm your selection!");
+                String input = basicmenu.getInput();
+                assert input != null;
+
+                if ("Quit".equals(input)) {
+                    System.out.println("GoodByeFace!");
+                    break;
+                } else {
+                    switch (input) {
+                        case "MainMenu":
+                            basicmenu = new MainMenu();
+                            break;
+                        case "CheckOut":
+                            basicmenu = new CheckOut();
+                            break;
+                        case "UserAccounts":
+                            basicmenu = new UserAccount();
+                            break;
+                        case "Return":
+                            basicmenu = new Return();
+                            break;
+                        case "Lend":
+                            if ("admin".equals(library.getLoginUser().getDuty())) {
+                                basicmenu = new Lend();
+                            }
+                        default:
+                            System.out.println("Please confirm your selection!");
+                            break;
+                    }
+
+                    basicmenu.doSomething(bibliotecaApp, input);
                 }
             }
 
@@ -57,140 +81,154 @@ public class BibliotecaApp {
 //        System.out.println("Hello, world!");
     }
 
-    public static void returnBook(String input) {
-        while(true) {
-            bibliotecaApp.welcomeMessage(input);
+    public static void showLoginUserAccounts() {
+        System.out.println("userName:" + library.getLoginUser().getName());
+        System.out.println("emailAddress:" + library.getLoginUser().getEmailAddress());
+        System.out.println("phoneNumber:" + library.getLoginUser().getPhoneNumber());
+        System.out.println("password:" + library.getLoginUser().getPassword());
+        System.out.println("duty:" + library.getLoginUser().getDuty());
 
-            String checkBook = getInput();
-            assert checkBook != null;
+        while("Quit".equals(basicmenu.getInput())) break;
+    }
 
-            if("Quit".equals(checkBook)){
-                System.out.println("GoodBye Return Book!");
-                break;
+    public static boolean login() {
+        int time = 0;
+        while(time++ < 3) {
+            System.out.println("you have " + (3 - time) + "chances.");
+            System.out.print("Please input your username and password:xxx-xxxxx.:");
+            String userinfo = basicmenu.getInput();
+
+            if("Quit".equals(userinfo)) return false;
+
+            String[] userInfo;
+            if ((userInfo = userinfo.split("-")).length==2 && library.isHaveGivenUser(userInfo[0], userInfo[1])) {
+                library.setLoginUser(library.returnPeopleByGivenUsernameAndPassword(userInfo[0], userInfo[1]));
+                return true;
             }
+        }
+        return false;
+    }
 
-            String[] bookInfo;
-            if( (bookInfo = checkBook.split(" ")).length != 3){
-                System.out.println("Please input your book's name,book's author,book's press to match!");
-                continue;
-            }
-            Book book = new Book(bookInfo[0], bookInfo[1], bookInfo[2]);
+    public ArrayList<String> getLibraryStorageBooksName() {
 
-            boolean isAvaliable = library.isHaveGivenBookLendBook(book);
-            if (isAvaliable) {
-                library.returnBook(book);
-                System.out.println("Thank you for returning the book.");
-                continue;
-            } else {
-                System.out.println("That is not a valid book to return");
+        ArrayList<String> booksName = new ArrayList<>();
+        for (int temp = 0; temp <= library.getCountsOfBooksInLibrary()-1; temp++) {
+            booksName.add(((Book)(library.getAllBooksFromLibrary().get(temp))).getName());
+        }
+
+        return booksName;
+    }
+
+    public void giveBooksDetail(String bookname) {
+        ArrayList<Book> books = library.getBooksByBookName(bookname);
+        if(books.isEmpty())
+            System.out.println("the book is invalid!");
+        else {
+            for(int temp=0; temp<books.size(); temp++) {
+                System.out.println("Book Name:" + books.get(temp).getName());
+                System.out.println("Book Author:" + books.get(temp).getAuthor());
+                System.out.println("Book Press:" + books.get(temp).getPress());
             }
         }
     }
 
-    public static void checkOutBook(String input) {
-        while(true) {
-            bibliotecaApp.welcomeMessage(input);
+    public ArrayList<String> getLibraryStorageMoviesName() {
+        ArrayList<String> moviesName = new ArrayList<>();
+        for (int temp = 0; temp <= library.getCountsOfMoviesInLibraryStorage() - 1; temp++) {
+            moviesName.add(((Movie) library.getAllMoviesFromLibrary().get(temp)).getName());
+        }
+        return moviesName;
+    }
 
-            String checkBook = getInput();
-            assert checkBook!= null;
-
-            if("Quit".equals(checkBook)){
-                System.out.println("GoodBye Check-Out Book!");
-                break;
-            }
-
-            String[] bookInfo;
-            if( (bookInfo = checkBook.split(" ")).length != 3){
-                System.out.println("Please input your book's name,book's author,book's press to match!");
-                continue;
-            }
-            Book book = new Book(bookInfo[0], bookInfo[1], bookInfo[2]);
-
-            boolean isAvaliable = library.isHaveGivenBookStorage(book);
-            if (isAvaliable) {
-                library.lendbook(book);
-                System.out.println("Thank you!Enjoy the book.");
-                continue;
-            } else {
-                System.out.println("That book is not available");
+    public void giveMoviesDetail(String moviename) {
+        ArrayList<Movie> movies = library.getMoviesByMovieName(moviename);
+        if(movies.isEmpty())
+            System.out.println("the movie is invalid!");
+        else {
+            for(int temp=0; temp<movies.size(); temp++) {
+                System.out.println("Movie Name:" + movies.get(temp).getName());
+                System.out.println("Movie Year:" + movies.get(temp).getYear());
+                System.out.println("Movie Director:" + movies.get(temp).getDirector());
+                System.out.println("Movie Rating:" + movies.get(temp).getRating());
             }
         }
     }
 
-    public static void mainMenu(String input) {
-        while(true) {
+    public void checkOutBook(Book book) {
+        if (library.isHaveGivenBookStorage(book) && library.lendbook(book)) {
+            System.out.println("Thank you!Enjoy the book.");
+        } else {
+            System.out.println("That book is not available");
+        }
+    }
 
-            bibliotecaApp.welcomeMessage(input);
-            viewFaceBook(mainmenu);
+    public void checkOutMovie(Movie movie) {
+        if (library.isHaveGivenMovieInMovieStorage(movie) && library.lendMovie(movie)) {
+            System.out.println("Thank you!Enjoy the movie.");
+        } else {
+            System.out.println("That movie is not available");
+        }
+    }
 
-            String mainMenuInput = getInput();
-            assert mainMenuInput != null;
+    public void returnBook(Book book) {
+        if (library.isHaveGivenBookLendBook(book) && library.returnBook(book)) {
+            System.out.println("Thank you for returning the book.");
+        } else {
+            System.out.println("That is not a valid book to return");
+        }
+    }
 
-            if("Quit".equals(mainMenuInput))  {
-                System.out.println("GoodBye MainMenu!");
-                break;
-            }
+    public void returnMovie(Movie movie) {
+        if (library.isHaveGivenMovieInLendMovies(movie) && library.returnMovie(movie)) {
+            System.out.println("Thank you for returning the book.");
+        } else {
+            System.out.println("That is not a valid book to return");
+        }
+    }
 
-            if(!mainmenu.contains(mainMenuInput))
-                System.out.println("Select a valid option!");
-            else {
-                bibliotecaApp.welcomeMessage(mainMenuInput);
-                ArrayList<String> booksName = new ArrayList<>();
-                for (int temp = 0; temp <= library.getCountsOfBooksInLibrary()-1; temp++) {
-                    booksName.add(((Book)library.getAllBooksFromLibrary().get(temp)).getName());
+    public ArrayList<String> getLibraryLendMoviesName() {
+        ArrayList<String> moviesName = new ArrayList<>();
+        for (int temp = 0; temp <= library.getCountsOfMoviesInLibraryLend() - 1; temp++) {
+            moviesName.add(((Movie) library.getAllMoviesFromLendMovies().get(temp)).getName());
+        }
+        return moviesName;
+    }
+
+    public ArrayList<String> getLibraryLendBooksName() {
+        ArrayList<String> moviesName = new ArrayList<>();
+        for (int temp = 0; temp <= library.getCountsOfBooksOutLibrary() - 1; temp++) {
+            moviesName.add(((Book) library.getAllBooksFromLendBooks().get(temp)).getName());
+        }
+        return moviesName;
+    }
+
+    public void giveMoviesOwnerDetail(String moviename) {
+        ArrayList<Movie> movies = library.getAllMoviesFromLendMovies();
+        if(movies.isEmpty())
+            System.out.println("the movie is invalid!");
+        else {
+            for(int temp=0; temp<movies.size(); temp++) {
+                if(moviename.equals(((Movie)movies.get(temp)).getName())) {
+                    System.out.println("Movie Name:" + movies.get(temp).getName());
+                    System.out.println("Movie's Owner name:" + movies.get(temp).getOwner().getName());
+                    System.out.println("Movie's Owner phonenumber:" + movies.get(temp).getOwner().getPhoneNumber());
                 }
-                viewFaceBook(booksName);
-
-                while(true) {
-
-                    String bookname = getInput();
-                    assert bookname != null;
-
-                    if ("Quit".equals(bookname)) {
-                        System.out.println("GoodBye List Books!");
-                        break;
-                    } else {
-                        ArrayList<Book> books = library.getBooksByBookName(bookname);
-                        if(books.isEmpty())
-                            System.out.println("the book is inavliable!");
-                        else {
-                            for(int temp=0; temp<books.size(); temp++) {
-                                System.out.println("BookName:" + books.get(temp).getName());
-                                System.out.println("BookAuthor:" + books.get(temp).getAuthor());
-                                System.out.println("BookPress:" + books.get(temp).getPress());
-                            }
-                        }
-                    }
-                }
             }
         }
     }
 
-    public static String getInput() {
-
-        String input = null;
-        System.out.print("Please input your operation(you could input Quit if you want to leave!):");
-
-        try {
-            input = new BufferedReader(new InputStreamReader(System.in)).readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return input;
-    }
-
-    private void welcomeMessage(String info) {
-        System.out.println("**********************************");
-        System.out.println(info);
-        System.out.println("**********************************");
-
-    }
-
-    public static void viewFaceBook(ArrayList<String> facebook){
-        for(int temp=0; temp<facebook.size(); temp++){
-            if(null != facebook.get(temp))
-            System.out.println(facebook.get(temp));
+    public void giveBooksOwnerDetail(String bookname) {
+        ArrayList<Book> books = library.getAllBooksFromLendBooks();
+        if(books.isEmpty())
+            System.out.println("the book is invalid!");
+        else {
+            for(int temp=0; temp<books.size(); temp++) {
+                if(bookname.equals(((Book) books.get(temp)).getName())) {
+                    System.out.println("Book Name:" + books.get(temp).getName());
+                    System.out.println("Book's Owner name:" + books.get(temp).getOwner().getName());
+                    System.out.println("Book's Owner phonenumber:" + books.get(temp).getOwner().getPhoneNumber());
+                }
+            }
         }
     }
 }
--- test/com/twu/biblioteca/testBibliotecApp.java
+++ test/com/twu/biblioteca/testBibliotecApp.java
@@ -4,17 +4,18 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class testBibliotecApp {
 
-    private BibliotecaApp biliotecaApp = new BibliotecaApp();
+    private BibliotecaApp bibliotecaApp = new BibliotecaApp();
     private Library library;
-    private Book book;
-
 
     @Before
-    public void Prepaire() {
+    public void Prepare() {
 
     }
 
@@ -23,6 +24,38 @@ public class testBibliotecApp {
         assertEquals(1, 1);
     }
 
+    @Test
+    public void couldReturnBooksNameFromLibrayStorageBooks() {
+      /*  when(library.getCountsOfBooksInLibrary()).thenReturn(2);
+        ArrayList<Book> tempBooks = new ArrayList<Book>(Arrays.asList(new Book("book1","",""),new Book("book2","","")));
+        when(library.getAllBooksFromLibrary()).thenReturn(tempBooks);
+ps:it doesn't work,i don't know why
+*/
 
+        ArrayList<String> expect = new ArrayList<>(Arrays.asList("book1","book2"));
+        ArrayList<String> result = bibliotecaApp.getLibraryStorageBooksName();
+        assertEquals(expect,result);
+    }
+
+    @Test
+    public void couldReturnMoviesNameFromLibraryStorageMovies() {
+        ArrayList<String> expect = new ArrayList<>(Arrays.asList("movie1","movie2"));
+        ArrayList<String> result = bibliotecaApp.getLibraryStorageMoviesName();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    public void couldReturnMoviesNameFromLibraryLendMovies() {
+        ArrayList<String> expect = new ArrayList<>(Arrays.asList("movie2"));
+        ArrayList<String> result = bibliotecaApp.getLibraryLendMoviesName();
+        assertEquals(expect, result);
+    }
+
+    @Test
+    public void couldReturnBooksNameFromLibraryLendBooks() {
+        ArrayList<String> expect = new ArrayList<>(Arrays.asList("book3"));
+        ArrayList<String> result = bibliotecaApp.getLibraryLendBooksName();
+        assertEquals(expect, result);
+    }
 
 }
