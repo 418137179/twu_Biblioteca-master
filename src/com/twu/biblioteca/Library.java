89a04f9bd89b9d1e348a90df848474311de72064
YangJing <418137179@qq.com>
2014-07-29 14:12:34 +0800

    add customer and loginuser attribute and its setter and getter for library.:Yangjing_20140729

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -9,18 +9,29 @@ public class Library {
 
     private ArrayList<Book> bookStorage = new ArrayList<Book>();
     private ArrayList<Book> lendBooks = new ArrayList<Book>();
+    private ArrayList<Movie> movieStorage = new ArrayList<Movie>();
+    private ArrayList<Movie> lendMovies = new ArrayList<Movie>();
+    private ArrayList<People> customer = new ArrayList<People>();
+    private People loginUser = new People();
 
     Library(){
-
+        customer.add(new People("student","emailaddress1","phonenumber1","password1","student"));
+        customer.add(new People("admin","emailaddress2","phonenumber2","password2","admin"));
     }
 
     Library(Library library){
         bookStorage.addAll(library.bookStorage);
         lendBooks.addAll(library.lendBooks);
+        movieStorage.addAll(library.movieStorage);
+        lendMovies.addAll(library.lendMovies);
+        customer.addAll(library.customer);
     }
 
     public void addBookToLibrary(Book book){
         bookStorage.add(book);
+    }
+    public void addMovieToLibrary(Movie movie){
+        movieStorage.add(movie);
     }
 
     public ArrayList<Book> getBooksByBookName(String bookname) {
@@ -57,6 +68,7 @@ public class Library {
         boolean result = false;
         if(isHaveGivenBookStorage(book1)){
             bookStorage.remove(book1);
+            book1.setBookOwner(loginUser);
             lendBooks.add(book1);
             result = true;
         }
@@ -87,9 +99,133 @@ public class Library {
         boolean result = false;
         if(isHaveGivenBookLendBook(book3)){
             bookStorage.add(book3);
+            book3.setBookOwner(new People("library","library","library","library","library"));
             lendBooks.remove(book3);
             result = true;
         }
         return result;
+    }
+
+    public ArrayList<Movie> getMoviesByMovieName(String name) {
+        ArrayList<Movie> result = new ArrayList<>();
+        for(int temp=0; temp<movieStorage.size(); temp++){
+            if(name.equals(movieStorage.get(temp).getName())){
+                result.add(movieStorage.get(temp));
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Movie> getMoviesByMoiveYear(String year) {
+        ArrayList<Movie> result = new ArrayList<>();
+        for(int temp=0; temp<movieStorage.size(); temp++){
+            if(year.equals(movieStorage.get(temp).getYear())){
+                result.add(movieStorage.get(temp));
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Movie> getMoviesByMoiveDirector(String director) {
+        ArrayList<Movie> result = new ArrayList<>();
+        for(int temp=0; temp<movieStorage.size(); temp++){
+            if(director.equals(movieStorage.get(temp).getDirector())){
+                result.add(movieStorage.get(temp));
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Movie> getMoviesByMoiveRating(int rating) {
+        ArrayList<Movie> result = new ArrayList<>();
+        for(int temp=0; temp<movieStorage.size(); temp++){
+            if(rating == movieStorage.get(temp).getRating()){
+                result.add(movieStorage.get(temp));
+            }
+        }
+        return result;
+    }
+
+    public boolean isHaveGivenMovieInMovieStorage(Movie movie1) {
+        return movieStorage.contains(movie1);
+    }
+
+    public boolean lendMovie(Movie movie) {
+        boolean result = false;
+        if(isHaveGivenMovieInMovieStorage(movie)){
+            movieStorage.remove(movie);
+            movie.setMovieOwner(loginUser);
+            lendMovies.add(movie);
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean isHaveGivenMovieInLendMovies(Movie movie) {
+        return lendMovies.contains(movie);
+    }
+
+    public ArrayList<Movie> getAllMoviesFromLibrary() {
+        return movieStorage;
+    }
+
+    public boolean returnMovie(Movie movie) {
+        boolean result = false;
+        if(lendMovies.contains(movie)){
+            lendMovies.remove(movie);
+            movie.setMovieOwner(new People("library","library","library","library","library"));
+            movieStorage.add(movie);
+            result = true;
+        }
+        return result;
+    }
+
+    public int getCountsOfMoviesInLibraryStorage() {
+        return movieStorage.size();
+    }
+
+    public int getCountsOfMoviesInLibraryLend() {
+        return lendMovies.size();
+    }
+
+    public boolean isHaveGivenUser(String username, String password) {
+        boolean result = false;
+        for(int temp=0; temp<customer.size(); temp++){
+            if(username.equals(customer.get(temp).getName()) && password.equals(customer.get(temp).getPassword())){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
+    public void setLoginUser(People people){
+         loginUser = new People(people);
+    }
+
+    public People returnPeopleByGivenUsernameAndPassword(String username, String password) {
+        People result = new People();
+
+        for(int temp=0; temp<customer.size(); temp++){
+            if(username.equals(customer.get(temp).getName()) && password.equals(customer.get(temp).getPassword())){
+                result = new People(customer.get(temp));
+                break;
+            }
+        }
+
+        return result;
+    }
+
+
+    public People getLoginUser() {
+        return loginUser;
+    }
+
+    public ArrayList<Book> getAllBooksFromLendBooks() {
+        return lendBooks;
+    }
+
+    public ArrayList<Movie> getAllMoviesFromLendMovies() {
+        return lendMovies;
     }
 }
--- test/com/twu/biblioteca/testLibrary.java
+++ test/com/twu/biblioteca/testLibrary.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by yangjing on 14-7-27.
@@ -18,6 +16,8 @@ public class testLibrary {
     private Book book1;
     private Book book2;
     private Book book3;
+    private Movie movie1;
+    private Movie movie2;
 
     private Library library = new Library();
     @Before
@@ -26,32 +26,36 @@ public class testLibrary {
         book2 = new Book("book2","author2","press2");
         book3 = new Book("book3","author3","press3");
 
+        movie1 = new Movie("movie1","1942","director1",1);
+        movie2 = new Movie("movie2","1943","director2",2);
+
         library.addBookToLibrary(book1);
         library.addBookToLibrary(book2);
         library.addBookToLibrary(book3);
         library.lendbook(book3);
+
+        library.addMovieToLibrary(movie1);
+        library.addMovieToLibrary(movie2);
+        library.lendMovie(movie2);
     }
 
     @Test
     public void couldGetBooksFromLibraryByBookName() {
-        String bookname = "book1";
-        ArrayList<Book> result = library.getBooksByBookName(bookname);
+        ArrayList<Book> result = library.getBooksByBookName("book1");
         ArrayList<Book> expect = new ArrayList<>(Arrays.asList(new Book(book1)));
         assertEquals(expect,result);
     }
 
     @Test
     public void couldGetBooksFromLibraryByBookAuthor() {
-        String bookauthor = "author1";
         ArrayList<Book> expect = new ArrayList<Book>(Arrays.asList(new Book(book1)));
-        ArrayList<Book> result = library.getBooksByBookAuthor(bookauthor);
+        ArrayList<Book> result = library.getBooksByBookAuthor("author1");
         assertEquals(expect,result);
     }
 
     @Test
     public void couldGetBooksFromLibraryByBookPress() {
-        String bookpress = "press1";
-        ArrayList<Book> result = library.getBooksByBookPress(bookpress);
+        ArrayList<Book> result = library.getBooksByBookPress("press1");
         ArrayList<Book> expect = new ArrayList<>(Arrays.asList(new Book(book1)));
         assertEquals(expect,result);
     }
@@ -119,6 +123,154 @@ public class testLibrary {
     public void couldReturnTrueIfReturnBookSuccessful() {
         boolean result = library.returnBook(book3);
         assertTrue(result);
+    }
+
+    @Test
+    public void couldReturnFalseIfReturnBookFailed() {
+        boolean result = library.returnBook(book2);
+        assertFalse(result);
+    }
+
+    @Test
+    public void couldReturnMoviesByGivenMovieName() {
+        ArrayList<Movie> expect = new ArrayList<Movie>(Arrays.asList(new Movie(movie1)));
+        ArrayList<Movie> result = library.getMoviesByMovieName("movie1");
+        assertEquals(expect, result);
+    }
+
+    @Test
+    public void couldReturnMoviesByGivenMovieYear() {
+        ArrayList<Movie> expect = new ArrayList<>(Arrays.asList(new Movie(movie1)));
+        ArrayList<Movie> result = library.getMoviesByMoiveYear("1942");
+        assertEquals(expect,result);
 
     }
+
+    @Test
+    public void couldReturnMoviesByGivenMoviedirector() {
+        ArrayList<Movie> expect = new ArrayList<>(Arrays.asList(new Movie(movie1)));
+        ArrayList<Movie> result = library.getMoviesByMoiveDirector("director1");
+        assertEquals(expect,result);
+
+    }
+
+    @Test
+    public void couldReturnMoviesByGivenMovieRating() {
+        ArrayList<Movie> expect = new ArrayList<>(Arrays.asList(new Movie(movie1)));
+        ArrayList<Movie> result = library.getMoviesByMoiveRating(1);
+        assertEquals(expect,result);
+
+    }
+
+    @Test
+    public void couldReturnTrueIfGivenMovieInLibraryStorage() {
+        boolean result = library.isHaveGivenMovieInMovieStorage(movie1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void couldReturnFalseIfGivenMovieOutLibraryStorage() {
+        Movie movie3 = new Movie("movie3","1944","director3",3);
+        boolean result = library.isHaveGivenMovieInMovieStorage(movie3);
+        assertFalse(result);
+    }
+
+    @Test
+    public void returnTrueWhenLendMovieSuccessful() {
+        boolean result = library.lendMovie(movie1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void returnFalseWhenLendMovieFailed() {
+        Movie movie3 = new Movie("movie3","1944","director3",3);
+        boolean result = library.lendMovie(movie3);
+        assertFalse(result);
+    }
+
+    @Test
+    public void couldReturnTrueIfGivenMovieInLibraryLend() {
+        boolean result = library.isHaveGivenMovieInLendMovies(movie2);
+        assertTrue(result);
+    }
+
+    @Test
+    public void couldReturnFalseIfGivenMovieOutLibraryLend() {
+        Movie movie3 = new Movie("movie3","1944","director3",3);
+        boolean result = library.isHaveGivenMovieInLendMovies(movie3);
+        assertFalse(result);
+    }
+
+    @Test
+    public void couldGetAllMoviesFromLibrary() {
+        ArrayList<Movie> expect = new ArrayList<>(Arrays.asList(movie1));
+        ArrayList<Movie> result = library.getAllMoviesFromLibrary();
+        assertEquals(expect,result);
+    }
+
+    @Test
+    public void couldReturnTrueIfReturnMovieSuccessful() {
+        boolean result = library.returnMovie(movie2);
+        assertTrue(result);
+    }
+
+    @Test
+    public void couldReturnFalseIfReturnMovieFailed() {
+        boolean result = library.returnMovie(movie1);
+        assertFalse(result);
+    }
+
+    @Test
+    public void couldReturnCountsOfMoviesInLibrayStorage() {
+        int result = library.getCountsOfMoviesInLibraryStorage();
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void couldReturnCountsOfMoviesInLibrayLend() {
+        int result = library.getCountsOfMoviesInLibraryLend();
+        assertEquals(1,result);
+    }
+
+    @Test
+    public void couldReturnTrueIfUserExisted() {
+        boolean result = library.isHaveGivenUser("student","password1");
+        assertTrue(result);
+    }
+
+    @Test
+    public void couldReturnFalseIfUserNotExisted() {
+        boolean result = library.isHaveGivenUser("name1","password1");
+        assertFalse(result);
+    }
+
+    @Test
+    public void couldReturnPeopleByGivenUsernameAndPassword() {
+        People expect = new People("student","emailaddress1","phonenumber1","password1","student");
+        People result = library.returnPeopleByGivenUsernameAndPassword("student","password1");
+        assertEquals(expect,result);
+    }
+
+    @Test
+    public void couldReturnLoginUser() {
+        People expect = new People("student","emailaddress1","phonenumber1","password1","student");
+        library.setLoginUser(expect);
+        People result = library.getLoginUser();
+        assertEquals(expect,result);
+    }
+
+    @Test
+    public void couldReturnBooksFromLendBooks() {
+        ArrayList<Book> expect = new ArrayList<>(Arrays.asList(book3));
+        ArrayList<Book> result = library.getAllBooksFromLendBooks();
+        assertEquals(expect,result);
+    }
+
+    @Test
+    public void couldReturnMoviesFromLendMovies() {
+        ArrayList<Movie> expect = new ArrayList<>(Arrays.asList(movie2));
+        ArrayList<Movie> result = library.getAllMoviesFromLendMovies();
+        assertEquals(expect,result);
+    }
+
 }
